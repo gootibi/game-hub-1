15d1e979c16312471e121cf83724c5ec56aae74e
gootibi <gootibi@gmail.com>
2024-06-12 00:35:56 +0200

    Implement infinite scrolling

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import React from "react";
 
 interface Props {
   gameQuery: GameQuery;
@@ -13,16 +14,25 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
 
-  const { data, error, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
 
   if (error) return <Text>{error.message}</Text>;
 
+  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) ?? 0;
+
   return (
-    <Box padding='10px'>
+
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      next={() => fetchNextPage()}
+      hasMore={!!hasNextPage}
+      loader={<Spinner />}
+    >
+
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding='10px'
         spacing={6}
       >
         {isLoading &&
@@ -42,12 +52,9 @@ const GameGrid = ({ gameQuery }: Props) => {
         )
         )}
       </SimpleGrid>
-      {hasNextPage && (
-        <Button marginY={5} disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
-          {isFetchingNextPage ? 'Loading ...' : 'Load More'}
-        </Button>
-      )}
-    </Box>
+
+    </InfiniteScroll>
+
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,7 +30,8 @@ const useGames = (gameQuery: GameQuery) =>
         }),
     getNextPageParam: (lastPage, allPage) => {
       return lastPage.next ? allPage.length + 1 : undefined;
-    }
+    },
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
   })
 
 export default useGames;
