e8d4fd79ad706d5869d3d633c934d49411590f4e
gootibi <gootibi@gmail.com>
2024-06-18 20:28:11 +0200

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import { Game } from '../hooks/useGames'
 import getCroppedImageUrl from '../services/image-url'
 import CriticScore from './CriticScore'
@@ -19,7 +19,11 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top}/></Heading>
+        <Heading fontSize='2xl'>
+          <Link to={`/games/${game.slug}`} key={game.id}>
+            {game.name}
+          </Link>
+          <Emoji rating={game.rating_top} /></Heading>
       </CardBody>
     </Card>
   )
