e26526b873490dbe7c28c4b7df2f7d6c46daab17
gootibi <gootibi@gmail.com>
2024-06-18 22:33:49 +0200

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react"
+import { Heading, Spinner } from "@chakra-ui/react"
 import { useParams } from "react-router-dom"
+import ExpandableText from "../components/ExpandableText"
 import useGame from "../hooks/useGame"
 
 const GameDetailPage = () => {
@@ -7,11 +8,11 @@ const GameDetailPage = () => {
     const { slug } = useParams()
 
     const { data: game, isLoading, error } = useGame(slug!)
-    
+
     if (isLoading) {
         return <Spinner />
     }
-    
+
     if (error || !game) {
         throw error.message
     }
@@ -19,7 +20,9 @@ const GameDetailPage = () => {
     return (
         <>
             <Heading>{game.name}</Heading>
-            <Text>{game.description_raw}</Text>
+            <ExpandableText>
+                {game.description_raw}
+            </ExpandableText>
         </>
     )
 }
