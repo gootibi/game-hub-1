6a795e4db506aaff4d3f1cc85dd7fc507da1e6bb
gootibi <gootibi@gmail.com>
2024-06-19 14:01:38 +0200

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { Heading, Spinner } from "@chakra-ui/react"
 import { useParams } from "react-router-dom"
 import ExpandableText from "../components/ExpandableText"
+import GameAttributes from "../components/GameAttributes"
 import useGame from "../hooks/useGame"
 
 const GameDetailPage = () => {
@@ -23,6 +24,7 @@ const GameDetailPage = () => {
             <ExpandableText>
                 {game.description_raw}
             </ExpandableText>
+            <GameAttributes game={game} />
         </>
     )
 }
